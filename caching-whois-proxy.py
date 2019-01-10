eb86ee817af6c8a73f2950a2df25fce6fb577447
Glen Dosey <doseyg@r-networks.net>
2019-01-09 23:01:55 -0500

    Add files via upload
    Added basic web server

--- caching-whois-proxy.py
+++ caching-whois-proxy.py
@@ -1,20 +1,59 @@
 #!/usr/bin/python
-## Glen Dosey <doseyg@r-networks.net>
-## 2019-01-09
-## MIT License
-##
 import sys, datetime
-import whois, json
+## This is the python-whois package; pip install python-whois
+import whois
 from elasticsearch import Elasticsearch, helpers
-import requests
-var = sys.argv[1]
+import requests, json
+from urlparse import urlparse
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
-def send_to_elasticsearch(myjson):
-	json_string = json.dumps(myjson)
+
+class webHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		try:
+			if self.path=="/":
+				self.send_response(200)
+				self.send_header('Content-type','text/html')
+				self.end_headers()
+				html = '<html><body>Submit your Whois query<br><form action="/api"><input type="text" name="query"><br><input type="submit"></form></body></html>'
+				self.wfile.write(html)
+			elif self.path.startswith("/api?query"):
+				query = urlparse(self.path).query
+				query_args = dict(args.split("=") for args in query.split("&"))
+				question = query_args['query']
+				answer = whois_lookup(question)
+				self.send_response(200)
+				self.send_header('Content-type','text/json')
+				self.end_headers()
+				self.wfile.write(json.dumps(answer,indent=4, sort_keys=True, default=str))
+
+			elif self.path.startswith("/api?cache"):
+				query = urlparse(self.path).query
+				query_args = dict(args.split("=") for args in query.split("&"))
+				whois_query = query_args['cache']
+				self.send_response(200)
+				self.send_header('Content-type','text/html')
+				self.end_headers()
+				html = 'success'
+				self.wfile.write(html)
+			else:
+				self.send_error(404,'Something went wrong. Query was: %s' % self.path)
+			return
+		except Exception as e:
+			self.send_error(500,'Something went wrong. Query was: %s' % e)
+
+	def do_POST(self):
+		self.send_error(404,'Something went wrong. You can\'t Post to this site. ')
+		return
+
+
+
+def send_to_elasticsearch(args,myjson):
+	json_string = json.dumps(myjson['_source'])
 	json_list = []
 	json_list.append(json_string)
 	es = Elasticsearch('http://elastic:changeme@elk_server:9200')
-	helpers.bulk(es,json_list,index='misp_hits', doc_type='generated')
+	helpers.bulk(es,json_list,index='whois_cache', doc_type='generated')
 
 def send_to_splunk(myjson):
 	SPLUNK_URI='https://splunk_server:8088/services/collector'
@@ -38,18 +77,35 @@ def load_disk_cache():
 	#print("DEBUG: DISK CACHE CONTENT:" + disk_cache_content)
 	return disk_cache_json
 
+#mem_cache = {}
+
+
+def whois_lookup(question):
+	if question in mem_cache.keys():
+		print("DEBUG: Mem_Cache hit")
+		results = mem_cache[question]
+	else:
+		print("DEBUG: Mem_Cache miss")
+		results = whois.whois(question)
+		#send_to_elasticsearch(results)
+		mem_cache[question] = results
+	disk_cache = open('whois.cache', 'w')  
+	disk_cache.write(json.dumps(mem_cache, default=str))  
+	disk_cache.close()
+	return results
+
+
+
+var = sys.argv[1]
 mem_cache = load_disk_cache()
 
-if var in mem_cache.keys():
-	print("DEBUG: Mem_Cache hit")
-	results = mem_cache[var]
+if var == '-d':
+	## Run the web server, I'll figure out how to background later
+	web_port=8080
+	server = HTTPServer(('', web_port), webHandler)
+	print 'Started httpserver on port ' , web_port
+	server.serve_forever()
 else:
-	print("DEBUG: Mem_Cache miss")
-	results = whois.whois(var)
-	#send_to_elasticsearch(results)
-	mem_cache[var] = results
-
-print(results)
-disk_cache = open('whois.cache', 'w')  
-disk_cache.write(json.dumps(mem_cache, default=str))  
-disk_cache.close()
+	## looks like run from command line
+	answer = whois_lookup(var)
+	print(answer)
